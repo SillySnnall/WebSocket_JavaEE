12f2e354fa67b937619149681f32187861fc220e
SillySnnall <sillysnnall@outlook.com>
2018-05-21 15:50:27 +0800

    完成

--- src/MWebSocketService.java
+++ src/MWebSocketService.java
@@ -5,8 +5,14 @@ import org.java_websocket.server.WebSocketServer;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MWebSocketService extends WebSocketServer {
+
+    private Map<String, WebSocket> webSocketMap = new HashMap<>();
+
 
     public MWebSocketService(int port) throws UnknownHostException {
         super(new InetSocketAddress(port));
@@ -18,16 +24,27 @@ public class MWebSocketService extends WebSocketServer {
 
     @Override
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+//        String address = webSocket.getRemoteSocketAddress().getAddress().getHostAddress();
+//        String message = String.format("(%s) <进入房间！>", address);
+//        sendToAll(message);
+//        System.out.println(message);
+
         String address = webSocket.getRemoteSocketAddress().getAddress().getHostAddress();
-        String message = String.format("(%s) <进入房间！>", address);
-        sendToAll(message);
-        System.out.println(message);
+        StringBuilder message = new StringBuilder("ip:");
+        webSocketMap.put(address, webSocket);
+        for (String key : webSocketMap.keySet()) {
+                message.append(key).append(",");
+        }
+        message.deleteCharAt(message.length() - 1);
+        sendToAll(message.toString());
+        System.out.println(message.toString());
     }
 
     @Override
     public void onClose(WebSocket webSocket, int i, String s, boolean b) {
         String address = webSocket.getRemoteSocketAddress().getAddress().getHostAddress();
         String message = String.format("(%s) <退出房间！>", address);
+        webSocketMap.remove(address);
         sendToAll(message);
 
         System.out.println(message);
@@ -39,8 +56,15 @@ public class MWebSocketService extends WebSocketServer {
         //服务端接收到消息
         String address = webSocket.getRemoteSocketAddress().getAddress().getHostAddress();
         String message = String.format("(%s) %s", address, s);
-        //将消息发送给所有客户端
-        sendToAll(message);
+        if(s.contains("ip:")){
+            String replace = s.replace("ip:", "");
+            String[] split = replace.split(",");
+            WebSocket webSocket1 = webSocketMap.get(split[0]);
+            webSocket1.send(split[1]);
+        }else {
+            //将消息发送给所有客户端
+            sendToAll(message);
+        }
         System.out.println(message);
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
@@ -18,18 +22,14 @@ public class Main {
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
-
-//        InputStreamReader in = new InputStreamReader(System.in);
-//        BufferedReader reader = new BufferedReader(in);
-
-//        while (true) {
-//            try {
-//                String msg = reader.readLine();
-//                socketServer.sendToAll(msg);
-//            } catch (Exception e) {
-//                e.printStackTrace();
+//        Timer timer = new Timer();
+//        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                System.out.println();// new Date()为获取当前系统时间
+//                socketServer.sendToAll("系统消息啊:" + df.format(new Date()));
 //            }
-//        }
-
+//        }, 0, 1000 * 10);
     }
 }
